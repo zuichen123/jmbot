74aae0d46b7807f5e3c8e6aef1330f663d302340
zuichen <627585966@qq.com>
2026-02-10 17:30:15 +0800

    清理环境

--- README.md
+++
@@ -1,3 +0,0 @@
-# jmbot
-# jmbot
-# jmbot
--- jmcomic/lib/python3.11/site-packages/common/postman/__init__.py
+++
@@ -1,6 +0,0 @@
-from .postman_api import *
-from .postman_impl import *
-from .postman_proxy import *
-
-ComponentRegistry.register_component(Postman, 'postman_key', globals().values(), False)
-ComponentRegistry.get_all_impl(Postman)['cffi'] = CurlCffiPostman
